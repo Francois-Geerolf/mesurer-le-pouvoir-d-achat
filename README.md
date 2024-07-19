42de44e93935502155cb9818d90269c9ede8e9c9
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2024-07-19 16:48:47 +0200

    update figure1

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Mesurer le pouvoir d'achat
 
+Ce dépôt met à disposition des codes de réplication pour le [document de travail en lien](https://fgeerolf.com/mesurer-le-pouvoir-d-achat.pdf).
+
+## Réplication
+
+Le dépôt contient de quoi répliquer les figures et les données du [document de travail en lien](https://fgeerolf.com/mesurer-le-pouvoir-d-achat.pdf):
+
+### Figure 1: IPC, IPCH et déflateur de la consommation
+
+[Code R](figure1.R)
+
+![FigureA1](figure1.png)
+
+### Figure A1 : Poids de l’enseignement dans l’IPC et dans l’IPCH.
+
+[Code R](figureA1.R)
+
+![FigureA1](figureA1.png)
---
+++ figure1.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+library(readxl)
+
+# IPC et IPCH --------
+
+figure1_idbanks <- c("001764363", "001762489")
+
+IPC_IPCH <- paste(figure1_idbanks, collapse = "+") |>
+  paste0("https://www.bdm.insee.fr/series/sdmx/data/SERIES_BDM/", i = _) |>
+  rsdmx::readSDMX() |>
+  tibble::as_tibble() |>
+  transmute(date = as.Date(paste0(TIME_PERIOD, "-01-01")),
+            OBS_VALUE = as.numeric(OBS_VALUE),
+            variable = case_when(grepl("harmonisé", TITLE_FR) ~ "IPCH",
+                                        T ~ "IPC"))
+
+# Déflateur de la consommation -------
+
+temp <- tempfile()
+
+curl::curl_download("https://www.insee.fr/fr/statistiques/fichier/8068592/T_CONSO_EFF_FONCTION.xlsx",
+              destfile = temp)
+
+deflateur <- read_excel(temp, skip = 3, sheet = "IPRIX2020") %>%
+  rename(fonction = ...1, variable = ...2) %>%
+  gather(year, OBS_VALUE, -fonction, -variable) %>%
+  filter(!is.na(OBS_VALUE),
+         variable %in% c("Dépense de consommation des ménages")) %>%
+  transmute(date = as.Date(paste0(year, "-01-01")),
+            OBS_VALUE,
+            variable)
+
+# Figure 1 ----------
+
+figure1 <- deflateur %>%
+  bind_rows(IPC_IPCH) %>%
+  group_by(variable) %>%
+  arrange(date) %>%
+  mutate(OBS_VALUE = 100*OBS_VALUE/OBS_VALUE[1])
+
+figure1 %>%
+  ggplot() + ylab("") + xlab("") + theme_minimal() +
+  geom_line(aes(x = date, y = OBS_VALUE, color = variable)) +
+  scale_x_date(breaks = seq(1920, 2100, 5) %>% paste0("-01-01") %>% as.Date,
+               labels = date_format("%Y")) +
+  theme(legend.position = c(0.3, 0.85),
+        legend.title = element_blank()) +
+  scale_y_log10(breaks = seq(0, 500, 5),
+                labels = dollar_format(accuracy = 1, prefix = ""))
+
+ggsave("figure1.png", width = 1.25*6, height = 1.25*3.375)
+ggsave("figure1.pdf", width = 1.25*6, height = 1.25*3.375)
